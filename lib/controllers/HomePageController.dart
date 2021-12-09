99e956aeb324006db4ee43f1965060b2e6d9eb73
Atlas8981 <sannchesda8981@gmail.com>
2021-12-09 12:35:21 +0700

    Add new class

---
+++ lib/controllers/HomePageController.dart
@@ -0,0 +1,14 @@
+import 'package:get/get.dart';
+
+class HomePageController extends GetxController{
+  RxList? recentViewItem;
+
+  @override
+  void onInit() {
+    super.onInit();
+  }
+
+  void getRecentViewItem(){
+
+  }
+}
